727172e210760f7edf459ced3908a8d1ad1b1943
Medalternativanat <medicinaalternativanatural26@gmail.com>
2026-04-06 16:49:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Nattiva App 🌿</h1>
-      <p>Seu app está funcionando 🚀</p>
+    <div>
+      <h1>Nattiva App</h1>
     </div>
   )
 }
