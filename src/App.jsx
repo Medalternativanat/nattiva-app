a8e92464b04e2667a5a0af6d7d0a31d5a1bd267a
Medalternativanat <medicinaalternativanatural26@gmail.com>
2026-04-06 17:24:32 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Sobre() {
+  return <h2>Sobre</h2>
+}
+
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Nattiva App</h1>
-      <p>Se você está vendo isso, React está funcionando.</p>
-    </div>
+    <BrowserRouter>
+      <div style={{ padding: 20 }}>
+        <h1>Nattiva App</h1>
+
+        <nav style={{ marginBottom: 20 }}>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/sobre">Sobre</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
