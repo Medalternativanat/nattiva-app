dfd49898d8309548aec09159b08c322d5bec8050
Medalternativanat <medicinaalternativanatural26@gmail.com>
2026-04-07 16:35:49 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage.jsx"
 import SobrePage from "./pages/SobrePage.jsx"
+import ResultPage from "./pages/ResultPage.jsx"
 import BottomNav from "./components/BottomNav"
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/sobre" element={<SobrePage />} />
+          <Route path="/resultado" element={<ResultPage />} />
         </Routes>
 
         <BottomNav />
