82acd798bac42cb0e8c7aa95d629bf96ce2f8441
Medalternativanat <medicinaalternativanatural26@gmail.com>
2026-04-09 17:35:31 -0300

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: "nodejs",
+};
+
 export default async function handler(req, res) {
   try {
     const response = await fetch("https://api.openai.com/v1/models", {
