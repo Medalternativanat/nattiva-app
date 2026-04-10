30f5dcc764bcaeb7ec41225a4678abd9a88c1987
Medalternativanat <medicinaalternativanatural26@gmail.com>
2026-04-10 11:01:55 -0300

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,5 +1,16 @@
 export default async function handler(req, res) {
-  return res.status(200).json({
-    etapa: "PASSO 1 OK"
-  });
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await response.json();
+
+    return res.status(200).json({
+      etapa: "PASSO 2 OK",
+      data: data
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      erro: error.message
+    });
+  }
 }
