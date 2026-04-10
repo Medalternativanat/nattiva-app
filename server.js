e8cf9ef8c8b5748c119429a7435d72e6e778492c
Medalternativanat <medicinaalternativanatural26@gmail.com>
2026-04-10 12:07:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ import express from "express";
 
 const app = express();
 
-app.get("/api/test", async (req, res) => {
-  res.json({
-    status: "SERVIDOR OK",
-  });
+app.get("/", (req, res) => {
+  res.send("RAILWAY OK");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ status: "OK" });
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Rodando na porta " + PORT);
 });
- 
